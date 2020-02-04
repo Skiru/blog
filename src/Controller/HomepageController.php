d6bae2488e777ee85623bcf7d80fb723327e9a85
mateusz Kozioł <mateusz.koziol15@gmail.com>
2020-02-04 19:43:51 +0100

    add redirect buttons

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -23,11 +23,6 @@ class HomepageController extends AbstractController
      */
     public function index(): Response
     {
-
-        $number = rand(0, 200);
-
-        return new Response(
-            '<html><body><h1>' . sprintf("1-1 number is %s", (string)$number) . '</h1></body></html>'
-        );
+        return $this->render('homepage/index.html.twig');
     }
 }
