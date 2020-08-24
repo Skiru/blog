42be3a1048f40c3955bf1002cde19391110cf95d
mateusz Kozioł <mateusz.koziol15@gmail.com>
2020-08-24 08:28:15 +0200

    Working frontend

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import '../css/app.css';
+import '../../public/assets/css/open-iconic-bootstrap.min.css';
+import '../../public/assets/css/animate.css';
+import '../../public/assets/css/owl.theme.default.min.css';
+import '../../public/assets/css/magnific-popup.css';
+import '../../public/assets/css/aos.css';
+import '../../public/assets/css/ionicons.min.css';
+import '../../public/assets/css/flaticon.css';
+import '../../public/assets/css/icomoon.css';
+import '../../public/assets/css/style.css';
+import '../../public/assets/css/purpleclouds.css';
+
